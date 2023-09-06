a080b4e9dfe81c5e2e1d902520028a6260922809
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2023-09-06 22:52:41 +0200

    feat: add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Section from '../components/Section';
+import PageHeader from '../components/PageHeader';
+import image404 from '../../public/images/404.jpg';
+
+export default function AboutUs() {
+    return (
+        <div className="container">
+            <div className="row align-items-center">
+                <div className="col-lg-3 col-md-1" />
+                <div className="col-lg-6 col-md-10">
+                    <div className="img-block">
+                        <Image
+                            src={image404}
+                            alt="Not found error"
+                            placeholder="blur"
+                            layout="responsive"
+                            sizes="(min-width: 1200px) 540px, (min-width: 992px) 450px, (min-width: 768px) 690px, (min-width: 576px) 510px, calc(100vw-30px)"
+                            quality="75"
+                        />
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-1" />
+            </div>
+        </div>
+    );
+}
