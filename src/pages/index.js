d8fe8ae63a81f2b6a7be7a82dea926580a178b51
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2023-09-07 22:58:23 +0200

    fix: add notification on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import Idea from '../components/Idea';
 import Conversations from '../components/Conversations';
 import WhyUs from '../components/WhyUs';
 import Opinions from '../components/Opinions';
+import Notification from '../components/Notification';
 
 export default function Home() {
     return (
         <>
+            <Notification showLink />
             <Banner />
             <Features />
             <WhyUs />
